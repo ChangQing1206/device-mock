f9ef1d7074d9261aea84e5974dc1efaa23b5ae62
CQ1999 <327058211@qq.com>
2022-02-27 23:04:00 +0800

    timeout解决响应

--- test-server.js
+++ test-server.js
@@ -1,14 +1,5 @@
 // mosca 服务端  不使用固定集合
 var mosca = require('mosca')
-var MongoClient = require('mongodb').MongoClient;
-var dtime = require('time-formater');
-var url = 'mongodb://localhost:27017/scene';
-var scene;
-MongoClient.connect(url, function (err, db) {
-	if(err) throw err;
-	console.log("mongo数据库已连接");
-	scene = db;
-})
 
 // 设备状态
 var deviceState = new Map();
@@ -24,40 +15,41 @@ var server = new mosca.Server({
 
 // 服务器启动
 server.on('ready', function() {
+  // 第一步
   console.log("mqtt server started");
 });
 // 服务器处理发布端的事情
 server.on('published', function(packet, client) {
+  // 第三步
   console.log("服务器接收客户端发布");
-  // 所有的操作都必须确保设备状态在线
+  console.log(packet); 
   
-  console.log(packet);
-  var topic = packet.topic; // {topic, payload} packet.payload.toString()
-  switch(topic) {
-    case "vistor/update_vistor_model":
-      // 更新游客数据时 插入游客表
-      var vistor = {};
-      var v = JSON.parse(packet.payload.toString());
-      // vistor.clientId = v.clientId;
-      vistor.identity = v.identity;
-      vistor.name = v.name;
-      vistor.bodyTem = v.bodyTem;
-      vistor.position = v.position;
-      vistor.date_time = dtime().format('YYYY-MM-DD HH:mm'); // 加上更新时间
-      console.log('vistor', vistor);
-      try {
-        scene.collection("vistors").insertOne(vistor, function(err, res) {
-          if(err) throw err;
-          console.log("插入成功");
-        })
-      } catch(err) {
-        console.log(err);
-      }
-      break;
-  }
+  // 充值的消息能发送成功 如何确保充值数据到达了设备
+  // 设备会响应充值状态 ，如果一定时间内无回应（不代表充值失败）但很大
+  // 概率失败，且在服务器处存储设备状态（仅在服务器存储状态，不能保证数据从服务器到设备的过程），两者结合充分保证充值过程
+  
   
 
-
+  /*
+    // 首次的消息
+    {
+      topic: '$SYS/v_FAWgd/new/clients',
+      payload: 'mqttjs_0e967544',
+      messageId: 'Co~Hwew',
+      qos: undefined,
+      retain: undefined
+    }
+    // 第二次的消息
+    {
+      topic: 'deposit/increaseDeposit/f10cf977',
+      payload: <Buffer 7b 22 6e 61 6d 65 22 3a 22 73 73 22 2c 22 69 64 65 6e 74 69 74 79 22 
+    3a 22 34 34 30 38 38 31 31 39 39 39 31 32 30 36 36 37 36 37 22 2c 22 64 65 70 6f ... 9 more bytes>,
+      messageId: 'k9vu1jd',
+      qos: 0,
+      retain: false
+    }
+    // 
+  */
 });
 // 服务器处理订阅端的事情
 server.on("subscribed", function(topic, client) {
@@ -71,6 +63,7 @@ server.on("unSubscribed", function(topic, client) {
 
 server.on("clientConnected", function(client) {
   console.log('client connect: ', client.id);
+  // 第二步
   // 存储设备状态
   var state = 1;
   deviceState.set(client.id, state)
